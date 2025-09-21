330a22ae49a636b4a7b6e8415651dc9b55b855b9
Niladri Ray Choudhury <niladriraychoudhury@outlook.com>
2025-09-21 16:58:15 +0530

    Chapter 1.6

---
+++ word_length_histogram.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int c, counter = 0, sum = 0;
+    int wordLength[100] = {0};
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            wordLength[counter] = sum;
+            counter++;
+            sum = 0;
+        } else {
+            ++sum;
+        }
+    }
+
+    printf("\nHistogram Results: \n");
+    for (int i=0; i<100; i++) {
+        printf("Length of word #%d: %d\n", i+1, wordLength[i]);
+    }
+
+    return 0;
+}
