47aee1fa1216d2620c4f20920d07ee032908aa59
Niladri Ray Choudhury <niladriraychoudhury@outlook.com>
2025-09-24 21:40:49 +0530

    Improved lower case converter

--- lower.c
+++ lower.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+# include <ctype.h>
 
-int lower(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return c + 32;
-    }
-    return c;
-}
+
+// int lower(int c) {
+//     if (c >= 'A' && c <= 'Z') {
+//         return c + 32;
+//     }
+//     return c;
+// }
 
 int main() {
     int c;
 
     while ((c = getchar()) != EOF) {
-        putchar(lower(c));
+        putchar(tolower(c));
     }
 
     return 0;
